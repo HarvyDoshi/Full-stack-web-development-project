36115e529260eb33b6404e881876ba35325ea51e
Harvy Doshi <harvymeenal@gmail.com>
2025-02-04 12:16:27 +0530

    Update Signup.tsx

--- frontend/src/pages/Signup.tsx
+++ frontend/src/pages/Signup.tsx
@@ -24,10 +24,16 @@ const Signup = () => {
     }
   };
   useEffect(() => {
-    if (auth?.user) {
-      return navigate("/chat");
-    }
-  }, [auth]);
+  if (auth?.user) {
+    setTimeout(() => navigate("/chat"), 0); // Ensures it's asynchronous but doesn't return a Promise
+  }
+}, [auth]);
+
+  // useEffect(() => {
+  //   if (auth?.user) {
+  //     return navigate("/chat");
+  //   }
+  // }, [auth]);
   return (
     <Box width={"100%"} height={"100%"} display="flex" flex={1}>
       <Box padding={8} mt={8} display={{ md: "flex", sm: "none", xs: "none" }}>

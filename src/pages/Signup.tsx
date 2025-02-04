f59ecc506739cb335ce8e10debee209aa96cb607
Harvy Doshi <harvymeenal@gmail.com>
2025-02-04 13:03:26 +0530

    Update Signup.tsx

--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -27,7 +27,7 @@ const Signup = () => {
     if (auth?.user) {
       return navigate("/chat");
     }
-  }, [auth]);
+  }, [auth,navigate]);
   return (
     <Box width={"100%"} height={"100%"} display="flex" flex={1}>
       <Box padding={8} mt={8} display={{ md: "flex", sm: "none", xs: "none" }}>

ceac7fa4d9ce932b889944bccf59bb4bb685e129
Harvy Doshi <harvymeenal@gmail.com>
2025-02-04 19:31:30 +0530

    Update token-manager.ts

--- backend/src/utils/token-manager.ts
+++ backend/src/utils/token-manager.ts
@@ -2,34 +2,34 @@ import { Request, Response, NextFunction } from "express";
 import jwt from "jsonwebtoken";
 import { COOKIE_NAME } from "./constants.js";
 
-// Create token function
 export const createToken = (id: string, email: string, expiresIn: string) => {
   const payload = { id, email };
-  const secretKey = process.env.JWT_SECRET || "defaultSecret";
-  return jwt.sign(payload, secretKey, { expiresIn });
+  const token = jwt.sign(payload, process.env.JWT_SECRET, {
+    expiresIn,
+  });
+  return token;
 };
 
-// Middleware to verify token
-export const verifyToken = (
+export const verifyToken = async (
   req: Request,
   res: Response,
   next: NextFunction
 ) => {
-  const token = req.signedCookies[COOKIE_NAME];
-
+  const token = req.signedCookies[`${COOKIE_NAME}`];
   if (!token || token.trim() === "") {
     return res.status(401).json({ message: "Token Not Received" });
   }
-
-  const secretKey = process.env.JWT_SECRET || "defaultSecret";
-
-  jwt.verify(token, secretKey, (err, decoded) => {
-    if (err) {
-      return res.status(401).json({ message: "Token Expired or Invalid" });
-    }
-
-    res.locals.jwtData = decoded;
-    next();
+  return new Promise<void>((resolve, reject) => {
+    return jwt.verify(token, process.env.JWT_SECRET, (err, success) => {
+      if (err) {
+        reject(err.message);
+        return res.status(401).json({ message: "Token Expired" });
+      } else {
+        resolve();
+        res.locals.jwtData = success;
+        return next();
+      }
+    });
   });
 };
 
